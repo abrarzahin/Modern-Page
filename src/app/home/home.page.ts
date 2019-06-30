d8495fa50166f5ca0b6894f839416097f30da1f8
abrar zahin <abrarzahin@yahoo.com>
2019-06-30 18:00:46 -0400

    5 aleart and add item

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +12,45 @@ export class HomePage {
 
   items=[];
 
-  constructor(private db: AngularFirestore, public afAuth: AngularFireAuth) {
+  constructor(private db: AngularFirestore, public afAuth: AngularFireAuth,
+    private alertController: AlertController) {
     
    
   }
+  async addItem() {
+    const alert = await this.alertController.create({
+      header: 'Please provide a item name',
+      inputs: [
+        {
+          name: 'itemName',
+          type: 'text',
+          placeholder: 'item name'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
 
+    await alert.present();
+
+    alert.onDidDismiss().then(result => {
+      console.log(result);
+      let text: string = result.data.values.itemName;
+      console.log(text);
+    });
+  
+}
 }
